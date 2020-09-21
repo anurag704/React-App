716ed1b5d34ee294a25f3176fbbeee77c4d4dd5c
anurag704 <anuragjha1093@gmail.com>
2020-09-21 19:04:39 +0530

    deploy

---
+++ build/precache-manifest.c6c13702d1d020160d7d9f3238563dc8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242450f9ab46eba5667d932a0e257ff7",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "c3ddf77cc991998466ea",
+    "url": "/React-App/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "6e929a8ff0f7fcd7ad1d",
+    "url": "/React-App/static/css/main.d1a143df.chunk.css"
+  },
+  {
+    "revision": "c3ddf77cc991998466ea",
+    "url": "/React-App/static/js/2.3f361c0c.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-App/static/js/2.3f361c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e929a8ff0f7fcd7ad1d",
+    "url": "/React-App/static/js/main.2d226cec.chunk.js"
+  },
+  {
+    "revision": "fab056ada119ad31482c",
+    "url": "/React-App/static/js/runtime-main.6d6e9462.js"
+  },
+  {
+    "revision": "81a343ac27711a797bbebd8a7b97e8ed",
+    "url": "/React-App/static/media/android.81a343ac.jpg"
+  },
+  {
+    "revision": "8e3d0464a6a94cf32aea88be2d1013b2",
+    "url": "/React-App/static/media/app.8e3d0464.jpg"
+  },
+  {
+    "revision": "02881b5530ce68f635d128da7d2c9c94",
+    "url": "/React-App/static/media/digital.02881b55.jpg"
+  },
+  {
+    "revision": "8537144c68fcda94ecc1a0e00a6dcc82",
+    "url": "/React-App/static/media/hero.8537144c.png"
+  },
+  {
+    "revision": "7487b91bd1e3d8f7826c7323fb702b29",
+    "url": "/React-App/static/media/img2.7487b91b.jpg"
+  },
+  {
+    "revision": "cc58b0467da8f007372956d64295d0dd",
+    "url": "/React-App/static/media/marketing.cc58b046.jpg"
+  },
+  {
+    "revision": "ac4f2feb34fdc900f224a7a9a321200b",
+    "url": "/React-App/static/media/s1.ac4f2feb.jpg"
+  },
+  {
+    "revision": "ea05ff32e084b02a65323373caf69993",
+    "url": "/React-App/static/media/software.ea05ff32.jpg"
+  }
+]);
